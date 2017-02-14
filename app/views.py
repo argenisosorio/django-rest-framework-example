01cbe181864bb942b061b67c90e3c2fc6c5ecbf1
aosorio <argenisosorio580@hotmail.com>
2017-02-14 00:30:48 -0400

    Agregadas vistas y urls para la vista simple

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-	
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class TestView(APIView):
+    """
+    Our First REST API for GET Requests
+    """
+ 
+    def get(self, request, format=None):
+        return Response({'detail': "Hello REST World"})
