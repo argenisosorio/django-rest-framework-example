2b8ebee6dca007fe9be3df07592f56c5a535d69e
Argenis Osorio <argenisosorio580@hotmail.com>
2017-08-09 00:18:23 -0400

    Modificado comentario en las urls.py

--- series/urls.py
+++ series/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     #url(r'^users/$', views.user_list),
     #url(r'^series/$', views.serie_list),
     #url(r'^series/(?P<pk>[0-9]+)/$', views.serie_detail),
-    ##### Using CBV ####
+    ##### Using CBV and CBV Generics ####
     url(r'^users/$', views.UsersList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^series/$', views.SeriesList.as_view()),
