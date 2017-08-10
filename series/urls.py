d5d518a6e8c19c69c636882d59dd6a2f0afc9bd2
Argenis Osorio <argenisosorio580@hotmail.com>
2017-08-10 16:08:00 -0400

    Arregladas las urls.py

--- series/urls.py
+++ series/urls.py
@@ -5,11 +5,15 @@ from series import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),    
-    ##### Using FBV ####
+    #####################
+    ##### Using FBV #####
+    #####################
     #url(r'^users/$', views.user_list),
     #url(r'^series/$', views.serie_list),
     #url(r'^series/(?P<pk>[0-9]+)/$', views.serie_detail),
-    ##### Using CBV and CBV Generics ####
+    ######################################
+    ##### Using CBV and CBV Generics #####
+    ######################################
     url(r'^users/$', views.UsersList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^series/$', views.SeriesList.as_view()),
