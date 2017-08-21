ef0e919d4bebde6d2b2d3b9f0bcc1794ed93dc75
Argenis Osorio <argenisosorio580@hotmail.com>
2017-08-21 06:20:21 -0400

    Agregado el método para detallar los usuarios

--- series/urls.py
+++ series/urls.py
@@ -4,18 +4,9 @@ from django.contrib import admin
 from series import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),    
-    #####################
-    ##### Using FBV #####
-    #####################
-    #url(r'^users/$', views.user_list),
-    #url(r'^series/$', views.serie_list),
-    #url(r'^series/(?P<pk>[0-9]+)/$', views.serie_detail),
-    ######################################
-    ##### Using CBV and CBV Generics #####
-    ######################################
-    url(r'^users/$', views.UsersList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-    url(r'^series/$', views.SeriesList.as_view()),
-    url(r'^series/(?P<pk>[0-9]+)/$', views.SerieDetail.as_view()),
+    url(r'^$', views.index, name='index'),
+    url(r'^users/$', views.user_list),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
+    url(r'^series/$', views.serie_list),
+    url(r'^series/(?P<pk>[0-9]+)/$', views.serie_detail),
 ]
--- series/views.py
+++ series/views.py
@@ -22,131 +22,7 @@ def index(request):
     """
     return HttpResponse("Hello, world!")
 
-##############################
-##### Using CBV Generics #####
-##############################
 
-class UsersList(generics.ListCreateAPIView):
-    """
-    Con permission_classes nos asegurarnos que solamente usuarios
-    autenticados tengan permisos para crear, actualizar y eliminar
-    series.
-    """
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-
-class SeriesList(generics.ListCreateAPIView):
-    queryset = Serie.objects.all()
-    serializer_class = SerieSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-
-class SerieDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Con permission_classes = IsOwnerOrReadOnly nos asegurarnos que solamente
-    el usuario que agrego una serie puedan editarla, sería un permiso a nivel de objeto.
-    """
-    queryset = Serie.objects.all()
-    serializer_class = SerieSerializer
-    #permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,)
-
-####################
-##### Using CBV ####
-####################
-
-'''
-class UsersList(APIView):
-    """
-    List all Users
-    """
-    def get(self, request, format=None):
-        usuarios = User.objects.all()
-        serializer = UserSerializer(usuarios, many=True)
-        return Response(serializer.data)
-
-
-class UserDetail(APIView):
-    """
-    Retrieve, update or delete a user instance.
-    """
-    def get_object(self, pk):
-        try:
-            return User.objects.get(pk=pk)
-        except User.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        usuario = self.get_object(pk)
-        serializer = UserSerializer(usuario)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        usuario = self.get_object(pk)
-        serializer = UserSerializer(usuario, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        usuario = self.get_object(pk)
-        usuario.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class SeriesList(APIView):
-    """
-    List all Series
-    """
-    def get(self, request, format=None):
-        series = Serie.objects.all()
-        serializer = SerieSerializer(series, many=True)
-        return Response(serializer.data)
-
-
-class SerieDetail(APIView):
-    """
-    Retrieve, update or delete a serie instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Serie.objects.get(pk=pk)
-        except Serie.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        serie = self.get_object(pk)
-        serializer = SerieSerializer(serie)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        serie = self.get_object(pk)
-        serializer = SerieSerializer(serie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        serie = self.get_object(pk)
-        serie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-'''
-
-#####################
-##### Using FBV #####
-#####################
-
-'''
 class JSONResponse(HttpResponse):
     """
     An HttpResponse that renders its content into JSON.
@@ -165,6 +41,33 @@ def user_list(request):
         usuarios = User.objects.all()
         serializer = UserSerializer(usuarios, many=True)
         return JSONResponse(serializer.data)
+
+
+@csrf_exempt
+def user_detail(request, pk):
+    """
+    Retrieve, update or delete a serie.
+    """
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(user, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return HttpResponse(status=204)
 
 
 @csrf_exempt
@@ -211,4 +114,3 @@ def serie_detail(request, pk):
     elif request.method == 'DELETE':
         serie.delete()
         return HttpResponse(status=204)
-'''
