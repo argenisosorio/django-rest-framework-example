d484b23929fd6e5215a6f52775982149c84fae47
Argenis Osorio <argenisosorio580@hotmail.com>
2017-08-21 06:47:16 -0400

    Limpiando el código de funciones innecesarias

--- series/urls.py
+++ series/urls.py
@@ -4,13 +4,7 @@ from django.contrib import admin
 from series import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),    
-    #####################
-    ##### Using FBV #####
-    #####################
-    #url(r'^users/$', views.user_list),
-    #url(r'^series/$', views.serie_list),
-    #url(r'^series/(?P<pk>[0-9]+)/$', views.serie_detail),
+    url(r'^$', views.index, name='index'),
     ######################################
     ##### Using CBV and CBV Generics #####
     ######################################
